12897e3fae42618b728f2bae364854ad579ed9c8
Maxim Domnin <recordclub94@yandex.ru>
2022-08-15 02:14:33 +0300

    -A

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+/* button */
+let editButton = document.querySelector('.profile__edit');
+let closeButton = document.querySelector('.popup__close-button');
+
+/* form */
+let formElement = document.querySelector('#form');
+let nameInput = document.querySelector('#nameInput');
+let jobInput = document.querySelector('#jobInput');
+
+/* other */
+let popupShow = document.querySelector('#popup')
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+/* main js code */
+
+
+function popupFormValue() {
+   nameInput.setAttribute('value', profileName.textContent);
+   jobInput.setAttribute('value', profileJob.textContent);
+}
+
+function openPopup() {
+   popupFormValue();
+   popupShow.classList.add('popup_opened');
+}
+
+function closePopup() {
+   popupShow.classList.remove('popup_opened');
+}
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+
+function formSubmitHandler(evt) {
+   evt.preventDefault();
+   profileName.textContent = nameInput.value;
+   profileJob.textContent = jobInput.value;
+   closePopup();
+}
+formElement.addEventListener('submit', formSubmitHandler);
